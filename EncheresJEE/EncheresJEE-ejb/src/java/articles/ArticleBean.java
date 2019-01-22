0038b6baffafaad1a076ac5e649c88e1cab40d0e
Laura-Weber <laura.weber57@laposte.net>
2019-01-22 11:29:19 +0100

    view all articles

--- EncheresJEE/EncheresJEE-ejb/src/java/articles/ArticleBean.java
+++ EncheresJEE/EncheresJEE-ejb/src/java/articles/ArticleBean.java
@@ -5,9 +5,17 @@
  */
 package articles;
 
+import bdd.BDDConnection;
+import java.sql.Connection;
 import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import users.Utilisateur;
@@ -21,8 +29,14 @@ import users.Utilisateur;
 @LocalBean
 public class ArticleBean implements Article{
     
+    private Connection con;
+
     public ArticleBean(){
-        
+        try {
+            con = DriverManager.getConnection("jdbc:derby://localhost:1527/train", "root", "pass");
+        } catch (SQLException ex) {
+
+        }    
     }
     
     @Override
@@ -78,7 +92,18 @@ public class ArticleBean implements Article{
     @Override
     public List<String> viewAll() {
         List<String> ret = new ArrayList<>(); ;
-        ret.add("nom : article1 , description : ceci est l'article 1 , prix_depart : 50 , date_fin : 12/12/19 , prix_actuel : 65 ,visible : 1 , id : 123");
+        //ret.add("nom : article1 , description : ceci est l'article 1 , prix_depart : 50 , date_fin : 12/12/19 , prix_actuel : 65 ,visible : 1 , id : 123");
+        try {
+            ResultSet rs = BDDConnection.getInstance().initialisationRequetePreparee(con, "", null).executeQuery("SELECT * FROM articles WHERE visible=true");
+
+            while(rs.next() != false){
+                ret.add(rs.getString(1) + " " + rs.getString(2) + " " + rs.getString(3) + " " + rs.getString(4) + " " + rs.getString(5) + " " + rs.getString(6) + " " + rs.getString(7) + " " + rs.getString(8) + " " + rs.getString(9));
+            }
+            rs.close();
+            
+        } catch (SQLException ex) {
+
+        }
         return ret;
     }
 
