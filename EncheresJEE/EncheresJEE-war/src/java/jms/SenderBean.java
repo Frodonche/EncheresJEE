9fad39d3d7dbdd8d1be05551a690b2628d7cacf5
Frodonche <guiguix3@hotmail.fr>
2019-01-30 23:29:51 +0100

    Send / Receive de messages JMS sur la meme appli

---
+++ EncheresJEE/EncheresJEE-war/src/java/jms/SenderBean.java
@@ -0,0 +1,53 @@
+package jms;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import javax.annotation.Resource;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.jms.JMSConnectionFactory;
+import javax.jms.JMSContext;
+import javax.jms.Queue;
+
+/**
+ *
+ * @author guigu
+ */
+@Named(value = "senderBean")
+@RequestScoped
+public class SenderBean {
+
+    @Resource(mappedName = "java:module/jms/myFacturationQueue")
+    private Queue myFacturationQueue;
+
+    @Inject
+    @JMSConnectionFactory("java:comp/DefaultJMSConnectionFactory")
+    private JMSContext context;
+
+    private String messageText;
+    
+    /**
+     * Creates a new instance of SenderBean
+     */
+    public SenderBean() {
+    }
+
+    public String getMessageText() {
+        return messageText;
+    }
+
+    public void setMessageText(String messageText) {
+        this.messageText = messageText;
+    }
+
+    public void sendMsgToQueue() {
+        System.out.println("Passage dans sendJMS "+ messageText);
+        context.createProducer().send(myFacturationQueue, messageText);
+    }
+    
+}
